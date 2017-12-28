f9d3b28a28772a820f3a0fd6e2537dbabd92292c
mlwebdev-react <mlwebdev-react@outlook.com>
2017-12-27 23:03:34 -0500

    Add CommentList componet (functuional)

---
+++ src/components/comment_list.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import CommentBox from "./comment_box";
+
+const CommentList = (props) => {
+  return (
+    <ul className="comment-list">
+    </ul>
+  )
+};
+
+export default CommentList;
