a6b36a3d041803e0b2b3a8d50b043ffb8b3300cd
mlwebdev-react <mlwebdev-react@outlook.com>
2017-12-28 18:55:49 -0500

    update

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,8 +1,10 @@
 import { SAVE_COMMENT } from "../actions/types";
-export default function (state = [], action) {
-  switch (action.type) {
+
+export default function(state = [], action) {
+  switch(action.type) {
     case SAVE_COMMENT:
       return [ ...state, action.payload ];
   }
+
   return state;
 }
