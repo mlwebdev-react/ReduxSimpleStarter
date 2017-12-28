7b0352ed8810244648331795b71035dee7767759
mlwebdev-react <mlwebdev-react@outlook.com>
2017-12-28 18:51:07 -0500

    update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import commentsReducers from "./comments";
 
 const rootReducer = combineReducers({
+  comments: commentsReducers
 });
 
 export default rootReducer;
