eff8e89f96c0c5fd2ad3d768509ccc74d9ee7337
mlwebdev-react <mlwebdev-react@outlook.com>
2017-12-27 23:01:05 -0500

    Refactor to be a form with onSubmit to handleSubmit.bind(this), preventDefault in handleSubmit, action="submit" on button

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,21 +1,29 @@
-import React, { Component } from "react";
+import React, { Component} from "react";
 
 export default class CommentBox extends Component {
   constructor(props){
   	super(props);
   	this.state = { comment: ""};
   }
+
   handleChange(event) {
     this.setState({ comment: event.target.value });
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ comment: "" });
+  }
+
   render() {
     return (
-      <div className="comment-box">
+      <form onSubmit={this.handleSubmit.bind(this)}
+        className="comment-box">
         <textarea
           value={this.state.comment}
           onChange={this.handleChange.bind(this)} />
-        <button>Submit Comment</button>
-      </div>
+        <button action="submit">Submit Comment</button>
+      </form>
     );
   }
 }
