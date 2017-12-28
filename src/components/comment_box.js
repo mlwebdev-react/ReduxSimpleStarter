1d64767c22bff9fe000a8e16e25d10d91473b87f
mlwebdev-react <mlwebdev-react@outlook.com>
2017-12-28 17:34:44 -0500

    Update

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -4,8 +4,9 @@ import * as actions from "../actions";
 
 class CommentBox extends Component {
   constructor(props){
-  	super(props);
-  	this.state = { comment: ""};
+    super(props);
+
+    this.state = { comment: ""};
   }
 
   handleChange(event) {
@@ -23,13 +24,16 @@ class CommentBox extends Component {
     return (
       <form onSubmit={this.handleSubmit.bind(this)}
         className="comment-box">
+        <h4>Add a comment</h4>
         <textarea
           value={this.state.comment}
           onChange={this.handleChange.bind(this)} />
-        <button action="submit">Submit Comment</button>
+        <div>
+          <button action="submit">Submit Comment</button>
+        </div>
       </form>
     );
   }
 }
 
-export default connect(null, actions)(CommentBox)
+export default connect(null, actions)(CommentBox);
